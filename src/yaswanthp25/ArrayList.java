4c32ab420d119e0d93ffbecf37c0d953a7402908
Yaswanth <yaswanth.pachhigolla@sayukth.com>
2026-05-29 09:33:57 +0530

    adad

--- src/yaswanthp25/ArrayList.java
+++ src/yaswanthp25/ArrayList.java
@@ -2,7 +2,7 @@ package yaswanthp25;
 
 public class ArrayList {
 	public static void main(String args[]) {
-		System.out.println("yaswanth mysoor");
+		System.out.println("yaswanth mysoor")
 		System.out.println("raj takrey")
 	}
 }
